b4cbc1202650d5a7a0d7332887cd0b88dfecbe57
Yoann Berenguer <38462796+yoyoberenguer@users.noreply.github.com>
2020-07-10 20:14:28 +0100

    Add files via upload

---
+++ ElasticCollision_REAL/setup_Project.py
@@ -0,0 +1,39 @@
+# distutils: extra_compile_args = -fopenmp
+# distutils: extra_link_args = -fopenmp
+
+
+# USE :
+# python setup_Project.py build_ext --inplace
+
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import numpy
+
+
+ext_modules = [
+
+    Extension("EC_SHARED", ["EC_SHARED.pyx"],
+              include_dirs=[numpy.get_include()],
+              extra_compile_args=['/openmp'],
+              extra_link_args=['/openmp']),
+
+    Extension("EC_REAL", ["EC_REAL.pyx"],
+              include_dirs=[numpy.get_include()],
+              extra_compile_args=['/openmp'],
+              extra_link_args=['/openmp']),
+
+    Extension("ECC", ["ECC.pyx"],
+              include_dirs=[numpy.get_include()],
+              extra_compile_args=['/openmp'],
+              extra_link_args=['/openmp'])
+
+
+]
+
+setup(
+    name="EC_GAME",
+    cmdclass={"build_ext": build_ext},
+    ext_modules=ext_modules,
+    include_dirs=[numpy.get_include()]
+)
